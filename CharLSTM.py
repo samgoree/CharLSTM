1dbd126bdefc89aed4fd8f972bb4061907b4e882
Sam Goree <sgoree@oberlin.edu>
2016-08-04 18:38:10 -0700

    Works, 5-strikes in place

--- CharLSTM.py
+++ CharLSTM.py
@@ -8,6 +8,7 @@ import numpy as np
 import sys
 import re
 import pickle
+from datetime import datetime
 
 
 class CharLSTM:
@@ -33,7 +34,7 @@ class CharLSTM:
         outputs_info=[dict(initial=np.int32([-1]),taps=[-1])] 
         + [dict(initial=layer.initial_hidden_state, taps=[-1]) for layer in self.model.layers if hasattr(layer, 'initial_hidden_state')])
 
-        self.forward_pass = theano.function([num_steps], [results[0]], updates=updates, allow_input_downcast=True)
+        self.forward_pass = theano.function([num_steps], [results[0].dimshuffle((1,0))[0]], updates=updates, allow_input_downcast=True)
 
         training_data = T.vector('training data') # list of character values less than num_possible_characters
 
@@ -47,7 +48,7 @@ class CharLSTM:
         outputs_info=[None] + [dict(initial=layer.initial_hidden_state, taps=[-1]) for layer in self.model.layers if hasattr(layer, 'initial_hidden_state')])
 
         prob_correct_v = results[0] # should be a vector of probabilities between 0 and 1
-        cost = -T.sum(T.log(prob_correct_v))
+        cost = -T.mean(T.log(prob_correct_v))
 
         u, gsums, xsums, lr, max_norm = create_optimization_updates(cost, self.model.params, method='adadelta')
 
@@ -55,28 +56,36 @@ class CharLSTM:
         self.validation_pass = theano.function([training_data], [cost], updates=updates, allow_input_downcast=True)
 
     # train the model on a numpy 2d array of character sequences
-    def train(self, dataset, batch_size=100, max_num_batches=100):
+    def train(self, dataset, batch_size=100, max_num_batches=5000):
         print("Training...")
         num_training_examples = (len(dataset) * 3) // 4
         num_validation_examples = len(dataset) - num_training_examples
         prev_cost = sys.maxsize
+        strikes=0
         for batch in range(1, max_num_batches):
             examples_to_train_on = np.random.choice(num_training_examples, batch_size, replace=False)
             for sample in dataset[examples_to_train_on]:
                 self.training_pass(sample)
-            if batch % 10 == 0:
-                print("Batch ", batch)
-                pickle.dump(self.model.params, 'batch' + str(batch) + '.p')
+            if batch % 100 == 0:
+                print("Minibatch ", batch * batch_size / num_training_examples)
+                params = []
+                for p in self.model.params:
+                    params += [p.get_value()]
+                pickle.dump(params, open('batch' + str(batch) + '.p', 'wb'))
                 cost = 0
                 examples_to_train_on = np.random.choice(num_validation_examples, batch_size, replace=False)
                 for sample in dataset[num_training_examples+examples_to_train_on]:
                     cost += self.validation_pass(sample)[0]
-                avg_cost = cost/batch_size
+                avg_cost = cost//batch_size
                 print("Cost: ", avg_cost)
                 if avg_cost > prev_cost:
-                    print("Stopping training")
-                    return
+                    strikes+=1
+                    print("Strike ",strikes)
+                    if strikes >= 5:
+                        print("Stopping training")
+                        return
                 else:
+                    strikes=0
                     prev_cost = avg_cost
 
 
@@ -103,14 +112,19 @@ def parse_text_file(filepath, delim='\n'):
     return np.array(lines), letter_number_dict
 
 def output_to_str(data, letter_number_dict):
-    number_letter_dict = dict((v, k) for k, v in map.iteritems())
+    number_letter_dict = dict((v, k) for k, v in letter_number_dict.items())
     s = ''
     for d in data:
-        s += number_letter_dict(d)
+        if d == len(letter_number_dict): return s
+        s += number_letter_dict[d]
     return s
 
 if __name__ == '__main__':
     data, lnd = parse_text_file(sys.argv[1], delim='\n|\.')
-    m = CharLSTM([50,100,50], len(lnd))
+    for i in range(len(data)):
+        data[i] = np.append(data[i], len(lnd))
+    m = CharLSTM([100,50,10], len(lnd)+1)
     m.train(data)
-    print(output_to_str(m.forward_pass(1000)[0], lnd))
+    output_file = open('output' + datetime.now().strftime('%m-%d %H:%M'), 'w')
+    for i in range(10):
+        output_file.write(output_to_str(m.forward_pass(1000)[0], lnd) + '\n')
